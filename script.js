222cc4a03271f850d9a5e9b9814be972415e85b4
Ry-F3 <rhysfeew@gmail.com>
2023-04-24 20:28:29 +0000

    Add js functions  to blackjack game

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
-let firstCard = Math.floor(Math.random() * 11) - 1;
-let secondCard = Math.floor(Math.random() * 11) + 2;
-
-let sum = (firstCard + secondCard);
-console.log(sum);
-
-let hasBlackJack = false;
-
-let isAlive = true;
-
-
-
-const assignSum = (firstCard= "") => {
-   const isSumEven = firstCard % 2 === 0;
-   return isSumEven ? 'even' : 'odd';
+function default_value() {
+    let num1 = "";
+    let num2 = "";
+    let sum = "";
 }
 
-console.log(assignSum(firstCard));
+function startGame() {
 
-if (assignSum(firstCard) === "odd"){
-  sum = sum + 2;
-} else {
-  sum = sum + 5;
+    let reset = default_value();
+    let num1 = Math.floor(Math.random() * 11) + 1; //number randomiser
+    let num2 = Math.floor(Math.random() * 11) + 1;
+
+    let sum = num1 + num2;
+
+    const assignSum = (num1 = "") => { //finding odd and even numbers
+        const isSumEven = num1 % 2 === 0;
+        return isSumEven ? 'even' : 'odd';
+    }
+
+    console.log(assignSum(num1));
+
+    if (assignSum(num1) === "odd") {
+        sum = sum;
+    } else {
+        sum = sum;
+    }
+
+    console.log(sum);
+
+    let message = "";
+
+    let messageEl = document.getElementById("message-el");
+
+    if (sum < 21) {
+        message = "Do you want to draw another card?";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+        if (sum >= 21) {
+           resetGame()
+        } else {
+           sum = sum;
+        }
+    }
+
+    let sumEl = document.getElementById("sum-el");
+    console.log(sumEl);
+
+    let cardEl = document.getElementById("card-el")
+
+     messageEl.innerHTML = message;
+     sumEl.innerHTML = "Sum: " + sum;
+
+     cardEl.textContent = "Cards: " + num1 + " & " + num2;
+
 }
 
-console.log(sum)
+console.log(messageEl);
 
-let message = "";
+function resetGame(){
+    console.log("---------reset all value---------");
+    reset = default_value();  
+    document.getElementById('sum-el').innerHTML = "Sum: " + 0; 
+    document.getElementById('card-el').innerHTML = "Cards: " + 0 + " " + "&" + " " + 0; 
 
-if (sum < 21) {
-    let message = "Do you want to draw another card?";
-} else if (sum === 21) {
-    let message = "Wohoo! You've got Blackjack!";
-    hasBlackJack = true;
-} else {
-    let message = "You're out of the game!";
-    isAlive = false;
 }
-
-console.log(isAlive);
-
-//cash out
-
-console.log(hasBlackJack);
-
-
